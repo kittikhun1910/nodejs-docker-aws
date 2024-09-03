42cd2fbcf2c6114cbc28e5976b31971ad0dd67cc
Kittikhun <tumgamemar@gmail.com>
2024-09-03 13:53:30 +0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:lts-alpine
+ENV NODE_ENV=production
+
+WORKDIR /app
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install --production
+
+COPY . .
+
+CMD ["node", "server/server.js"]
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
